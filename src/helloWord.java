592a92e9ed496af2eb5513be1c792a3734ada286
qixiangbo <1986234350@qq.com>
2020-09-14 16:50:33 +0800

    Initial commit

---
+++ src/helloWord.java
@@ -0,0 +1,73 @@
+import java.util.ArrayList;
+
+public class helloWord {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        ArrayList arrayList=new ArrayList();
+    //psvm
+   /* public static void main(String[] args) {
+
+    }*/
+        //sout
+        System.out.println();
+        //number1.sout
+        /*int number1;
+        System.out.println(number1);*/
+        //fori
+        for (int i = 0; i <5; i++) {
+
+        }
+        //iter
+        for (Object o : arrayList) {
+
+        }
+        //itar
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+
+        }
+        
+        ArrayList list=new ArrayList();
+        list.add(123);
+        list.add(456);
+        list.add(789);
+        //list.for
+        for (Object o : list) {
+            
+        }
+        //list.fori
+        for (int i = 0; i < list.size(); i++) {
+            
+        }
+        //list.forr  倒序遍历
+        for (int i = list.size() - 1; i >= 0; i--) {
+            
+        }
+        //ifn
+        if (list == null) {
+
+        }
+        //inn
+        if (list != null) {
+
+        }
+        //xxx.nn   xxx.null
+        if (list != null) {
+
+        }
+        if (list == null) {
+
+        }
+
+    }
+    public void method(){
+        //这是单
+    }
+    //prsf
+    private static final Customer cust=new Customer();
+    //psfi
+    public static final int num=1;
+    //psfs
+    public static final String address="s4515s";
+
+}
